d3210c8b8b7944971d9889bc501622c4988443e1
Gerhard Popp <gerhrad@poppgerhard.at>
2021-12-20 15:24:45 +0100

    update template controller

--- classes/controller/TemplatesController.php
+++ classes/controller/TemplatesController.php
@@ -8,67 +8,60 @@ class TemplatesController extends BaseController {
 	protected $template_folder = AUTOCAMPAIGNER_DIR . '/email_templates/';
 
 	protected $endpoints = [
-		'list'   => 'https://api.createsend.com/api/v3.2/clients/{clientid}/templates.json',
-		'create' => 'https://api.createsend.com/api/v3.2/templates/{clientid}.json',
-		'update' => 'https://api.createsend.com/api/v3.2/templates/{templateid}.json',
-		'details'    => 'https://api.createsend.com/api/v3.2/templates/{templateid}.json',
+		'list'    => 'https://api.createsend.com/api/v3.2/clients/{clientid}/templates.json',
+		'create'  => 'https://api.createsend.com/api/v3.2/templates/{clientid}.json',
+		'update'  => 'https://api.createsend.com/api/v3.2/templates/{templateid}.json',
+		'details' => 'https://api.createsend.com/api/v3.2/templates/{templateid}.json',
 	];
-
 
 
 	function is_saved_on_cm( $template_name ) {
 
-		$description = $this->template_description($template_name);
+		$description = $this->template_description( $template_name );
 
-		if( empty($description->TemplateID)) return false;
+		if ( empty( $description->TemplateID ) ) {
+			return false;
+		}
 
-		$details = $this->details();
+		$details = $this->details($description->TemplateID);
 
-		if(empty($details->Name)) return false;
+		if ( empty( $details->Name ) ) {
+			return false;
+		}
 
 		return $description;
 
 	}
 
-	public function template_description($name){
 
-		$json = file_get_contents( $this->template_folder . $name .'/description.json' );
-		return json_decode($json);
+	public function create_or_update_on_cm( $template_name ) {
 
-	}
+		$description = $this->is_saved_on_cm( $template_name );
 
-	
-
-	public function get_existing_templates() {
-
-		$templates = [];
-
-		$dirs = glob( AUTOCAMPAIGNER_DIR . '/email_templates/*' );
-
-		foreach ( $dirs as $dir ) {
-			if (  file_exists($dir . '/description.json') && file_exists( $dir . '/index.html' ) ){
-				$name        = explode( '/', $dir );
-				$templates[] = array_pop( $name );
-			}
+		if ( $description ) {
+			return $this->update_template_on_cm( $template_name, $description->TemplateID );
 		}
 
-		return $templates;
-	}
+		$description = $this->template_description($template_name);
+		$template_id = $this->create_template_on_cm( $template_name );
 
-	public function create_or_update_on_cm($template_name){
+		if($template_id){
 
-		$description = $this->is_saved_on_cm($template_name);
-
-		if($description){
-			return  $this->update_template_on_cm($template_name, $description->TemplateID) ;
+			$description->TemplateID = $template_id;
+			file_put_contents( $this->template_folder . $template_name . '/description.json', json_encode( $description ) );
 		}
-
-		$template_id = $this->create_template_on_cm($template_name);
-
-		$description->TemplateID = $template_id;
-		file_put_contents( $this->template_folder . $template_name . '/description.json',  json_encode($description)  );
 
 		return $template_id;
+
+	}
+
+	public function create_template_on_cm( $template_name ) {
+
+		return $this->call(
+			$this->get_endpoint( $this->endpoints['create'] ),
+			'post',
+			$this->request_body( $template_name )
+		);
 
 	}
 
@@ -84,20 +77,12 @@ class TemplatesController extends BaseController {
 	}
 
 
-	public function create_template_on_cm( $template_name ) {
-
-		return $this->call(
-			$this->get_endpoint( $this->endpoints['create'] ),
-			'post',
-			$this->request_body( $template_name )
-		);
-
-	}
-
 	public function request_body( $name ) {
 
+		$description = $this->template_description($name);
+
 		$body = [
-			'Name'        => $name,
+			'Name'        => $description->Name,
 			'HtmlPageURL' => AUTOCAMPAIGNER_URL . '/email_templates/' . $name . '/index.html',
 		];
 
@@ -114,6 +99,31 @@ class TemplatesController extends BaseController {
 
 		return file_exists( AUTOCAMPAIGNER_DIR . '/email_templates/' . $template_name . '/images.zip' );
 
+	}
+
+	public function template_description( $name ) {
+
+		$json = file_get_contents( $this->template_folder . $name . '/description.json' );
+
+		return json_decode( $json );
+
+	}
+
+
+	public function get_existing_templates() {
+
+		$templates = [];
+
+		$dirs = glob( AUTOCAMPAIGNER_DIR . '/email_templates/*' );
+
+		foreach ( $dirs as $dir ) {
+			if ( file_exists( $dir . '/description.json' ) && file_exists( $dir . '/index.html' ) ) {
+				$name        = explode( '/', $dir );
+				$templates[] = array_pop( $name );
+			}
+		}
+
+		return $templates;
 	}
 
 
